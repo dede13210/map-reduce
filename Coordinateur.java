0b18ae645153722c8490be5b610579fa58a881d6
adam <dede13210@gmail.com>
2023-12-04 14:10:49 +0100

    feature read txt

--- Coordinateur.java
+++ Coordinateur.java
@@ -1,2 +1,22 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Coordinateur {
+    public static void main(String args[]){
+        try{
+            // Le fichier d'entrée
+            FileInputStream file = new FileInputStream("bible.txt");
+            Scanner scanner = new Scanner(file);
+            System.out.println(scanner.toString());
+
+            scanner.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
+
