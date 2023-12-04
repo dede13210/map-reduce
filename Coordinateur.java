3f597731d02c75fb7827e85b6add5afe12f2b765
hug0be <83722580+hug0be@users.noreply.github.com>
2023-12-04 15:16:11 +0100

    [FEATURE] Split text

--- Coordinateur.java
+++ Coordinateur.java
@@ -2,6 +2,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class Coordinateur {
     public static String read(String filepath) throws FileNotFoundException {
@@ -10,6 +11,20 @@ public class Coordinateur {
         } catch (IOException e) {
             throw new FileNotFoundException("Le fichier \"" + filepath + "\" n'existe pas");
         }
+    }
+
+    public static String[] split(String txt, int nbBlocks) {
+        String[] lines = txt.split("\n");
+        int linesPerBlock = (int)Math.ceil((double)lines.length / nbBlocks);
+        String[] result = new String[nbBlocks];
+
+        for (int i = 0; i < nbBlocks; i++) {
+            int startIdx = i * linesPerBlock;
+            int endIdx = (i == nbBlocks - 1) ? lines.length : (i + 1) * linesPerBlock;
+            result[i] = String.join("\n", Arrays.copyOfRange(lines, startIdx, endIdx));
+        }
+
+        return result;
     }
 }
 
--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
 import java.io.FileNotFoundException;
-
 public class Main {
-
-    public static void main(String args[]){
+    public static int nbMap = 231;
+    public static void main(String[] args){
         String txt;
         try {
             txt = Coordinateur.read("bible.txt");
-            System.out.println(txt);
         } catch (FileNotFoundException ex) {
             System.out.println(ex.getMessage());
+            return;
         }
+
+        String[] blocks = Coordinateur.split(txt, nbMap);
     }
 }
