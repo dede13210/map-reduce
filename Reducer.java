66f5cc5aca528d58fddc4e14f4f6e90758d9b725
adam <dede13210@gmail.com>
2023-12-22 10:09:58 +0100

    [FIX] merge map in reducer

--- Reducer.java
+++ Reducer.java
@@ -1,11 +1,19 @@
+import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 public class Reducer {
     public Map<String,Integer> reduce(List<Map<String,Integer>> mapList){
-        Map<String,Integer> result = new HashMap<>();
-        for (Map<String,Integer> countMap:mapList){
-            result.putAll(countMap);
+        //merge map in entry
+        HashMap<String, Integer> result = new HashMap<>(mapList.get(0));
+        for (int i=1;i<mapList.size();i++){
+            for (String word:mapList.get(i).keySet()){
+                if(result.containsKey(word)){
+                    result.put(word,result.get(word)+mapList.get(i).get(word));
+                }else {
+                    result.put(word,mapList.get(i).get(word));
+                }
+            }
         }
         return result;
 
