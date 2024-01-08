2fa6ef5aa49728f5354210e74d7615358bf43358
hug0be <83722580+hug0be@users.noreply.github.com>
2024-01-08 23:47:40 +0100

    [INTERNAL] Better AlphabeticStrategy

--- AlphabeticStrategy.java
+++ AlphabeticStrategy.java
@@ -1,18 +1,8 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class AlphabeticStrategy implements ShuffleStrategy{
-    private final ArrayList<String> aphabeticList = new ArrayList<>(Arrays.asList("a","à", "b", "c", "d", "e","é", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
     @Override
     public int shuffle(String word, int nbReducers) {
         // TODO : Convert accents to simple letter
-        if(nbReducers==1)
-            return 0;
-//        System.out.println( word);
-        String firstChar = String.valueOf(word.charAt(0));
-        if(!aphabeticList.contains(firstChar)){
-            return 15%(nbReducers-1);
-        }
-        return aphabeticList.indexOf(firstChar)%(nbReducers-1);
+        if(nbReducers==1) return 0;
+        return word.charAt(0)%(nbReducers-1);
     }
 }
